2204f66a237186809964702916d11c1614ef35a2
sid94 <sid.kolhapure94@gmail.com>
2020-05-15 15:52:58 -0400

    Create Patch File

--- submit/task1/JPMC-tech-task-1-py3/client_test.py
+++ submit/task1/JPMC-tech-task-1-py3/client_test.py
@@ -1,23 +1,36 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
-
+    ]   
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+ quote['top_ask']['price'])/2))
+  
+  
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+ quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  
+  def test_getRatio(self):
+      data = [
+        {'abc_price' : 114.25, 'def_price' : 112.035 , 'ratio' : 1.0197706073994734},
+        {'abc_price' : 117.38, 'def_price' : 116.325 , 'ratio' : 1.0090694175800559},
+        {'abc_price' : 117.38, 'def_price' : 0 , 'ratio' : None}
+      ]
+      for val in data:
+            self.assertEqual(getRatio(val['abc_price'],val['def_price']),val['ratio'])
+      
 
 
 
